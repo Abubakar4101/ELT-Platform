91920315fb16b18755e0664b57149afcb1361fac
M.Abubakar Siddique <dev.abubakarsiddique@gmail.com>
2024-07-31 22:59:31 +0500

    configuration has been done

---
+++ src/config/db.js
@@ -0,0 +1,21 @@
+// db.js
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+/**
+ * Connect to MongoDB using Mongoose
+ */
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
